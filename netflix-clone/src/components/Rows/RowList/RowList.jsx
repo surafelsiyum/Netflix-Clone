8e2b024eb6d991d408155bfb4b3cda086654def8
suras <surafelsiyum50@gmail.com>
2025-10-26 23:10:56 +0300

    Deploying

--- netflix-clone/src/components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -11,47 +11,47 @@ const RowList = () => {
       <Row
         title="Trending Now"
         fetchUrl={requests.getTrending}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Top Rated Movies"
         fetchUrl={requests.getTopRatedMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Upcoming Movies"
         fetchUrl={requests.getUpcommingMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Action Movies"
         fetchUrl={requests.getActionMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Comedy Movies"
         fetchUrl={requests.getComedyMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Horror Movies"
         fetchUrl={requests.getHorrorMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Romance Movies"
         fetchUrl={requests.getRomanceMovies}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="Documentaries"
         fetchUrl={requests.getDocumentaries}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
       <Row
         title="TV Shows"
         fetchUrl={requests.getTvShow}
-        // isLargeRow={true}
+        isLargeRow={true}
       />
     </>
   );
